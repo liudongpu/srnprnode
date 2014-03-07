5af0dd3b860e9bf8adfaa8261366f5a9d134621e
srnpr <srnpr@srnprmacmini>
2014-03-07 23:19:33 +0800

    a

--- test/routes/index.js
+++ test/routes/index.js
@@ -1,12 +1,37 @@
-
 /*
  * GET home page.
  */
 
-var a=1;
+var a = 1;
 
-exports.index = function(req, res){
+exports.index = function(req, res) {
+
+	a = a + 1;
 	
-	a=a+1;
-  res.render('index', { title: 'Express'+a });
+	get_http();
+	
+	res.render('index', {
+		title : 'Express' + a
+	});
+};
+
+var get_http = function() {
+
+	var http = require('http');
+	var options = {
+		host : 'www.baidu.com',
+		path : '/',
+		method : 'GET',
+		headers : {
+			'Accept' : 'text/html'
+		}
+	};
+	var req = http.request(options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function(data) {
+			console.log(data);
+		});
+	});
+	req.end();
+
 };
